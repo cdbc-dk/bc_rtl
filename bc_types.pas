bfdb260faea23e0193d0b129059d6b1990eef70d
Benny Christensen <bc@hp.cdbc>
2021-02-27 10:48:39 +0100

    Addition to types

--- bc_types.pas
+++ bc_types.pas
@@ -1,39 +1,49 @@
 
-
-        {***************************************************************
-        * Unit name : bc_types.pas                                     *
-        * Copyright : (c) 2020 cdbc.dk                                 *
-        * Programmer: Benny Christensen /bc                            *
-        * Created   : 2020.04.18 /bc: TVersion created.                *
-        * Updated   : 2020.04.20 /bc: Refactored the math in both      *
-        *                             TVersion.EncodeFromString and    *
-        *                             TVersion.DecodeToString. it      *
-        *                             now relies on pointer magic :-)  *
-        *                             Implementation moved to include. *
-        *                                                              *
-        ****************************************************************
-        *                                                              *
-        * Abstract:                                                    *
-        * Provides different types for use in bc-codelibrary,          *
-        * sort of a global type-repository.                            *
-        *                                                              *
-        *                                                              *
-        *                                                              *
-        ***************************************************************}
+{**************************************************************************
+* Unit name : bc_types.pas                                                *
+* Copyright : (c) 2020-2021 cdbc.dk                                       *
+* Programmer: Benny Christensen /bc                                       *
+* Created   : 2020.04.18 /bc: TVersion created.                           *
+* Updated   : 2020.04.20 /bc: Refactored the math in both                 *
+*                             TVersion.EncodeFromString and               *
+*                             TVersion.DecodeToString. it                 *
+*                             now relies on pointer magic :-)             *
+*                             Implementation moved to include.            *
+*           : 2021.02.26 /bc: Implemented the observer pattern            *
+*                             Headers here and implementation             *
+*                             in observer.inc                             *
+*                                                                         *
+***************************************************************************
+*                                                                         *
+* Abstract:                                                               *
+* Provides different types for use in bc-codelibrary,                     *
+* sort of a global type-repository.                                       *
+*                                                                         *
+***************************************************************************
+* License:                                                                *
+* Beer-license, if we meet one day, you'll buy me a beer :-)              *
+* I'm NOT liable for anything!!! Use at your own risk!!!                  *
+*                                                                         *
+**************************************************************************}
 
 unit bc_types;
 {$mode objfpc}{$H+}
-{.$define use_lcltype}
+{$define debug}
+
 interface
 uses
-  Classes, SysUtils;
+  Classes, SysUtils,
+  RtlConsts;
   {$ifdef use_lcltype}
 
   {$else}
 
   {$endif}
 const
-  UnitVersion = '2.20.04.2020'; { TVersion uses this }
+  UnitVersion = '01.02.27.2021'; { 2.nd version }
+  { GUID for IbcSubject and IbcObserver }
+  SGUIDIbcSubject  = '{A036909B-DB8A-4426-843E-24DC452A7100}';
+  SGUIDIbcObserver = '{D013282B-CA63-497D-AF2D-BF10D2C34A82}';
 
 type
   { *** TVersion *** }
@@ -54,7 +64,52 @@ type
     property AsString: string read get_AsString;
   end; { TVersion }
 
+  { Notification operations : }
+  { Subject has changed, is freed, item added to/deleted from list, custom event. }
+  TbcSubjectOperation = (ooChange,ooFree,ooAddItem,ooDeleteItem,ooCustom);
+  {$interfaces corba} { NO reference counting! }
+  { IbcSubject to be observed }
+  IbcSubject = interface [SGUIDIbcSubject]
+    { attach a new observer }
+    Procedure bcAttachObserver(anObserver: TObject);
+    { Detach an observer }
+    Procedure bcDetachObserver(anObserver: TObject);
+    { Notify all observers of a change. }
+    Procedure bcNotifyObservers(aSender: TObject;anOperation: TbcSubjectOperation;aData: pointer);
+  end; { IbcSubject }
+  { IbcObserver }
+  IbcObserver = interface [SGUIDIbcObserver]
+    { Called by subject when observers are notified. }
+    Procedure bcSubjectChanged(aSender: TObject;anOperation: TbcSubjectOperation;aData: pointer); virtual;
+  end;
+  {$interfaces com} { reference counted! }
+  { TSubject to be observed ~ observer pattern }
+  TbcSubject = class(TObject,IbcSubject)
+  private
+    fObservers: TFPList;
+  protected
+    fOwner: TObject;
+    fExtra: ptruint;
+  public
+    constructor Create(anOwner: TObject); { the subject object you wish to monitor }
+    destructor Destroy; override;
+    Procedure bcAttachObserver(anObserver: TObject); { attach a new observer }
+    Procedure bcDetachObserver(anObserver: TObject); { detach an observer }
+    Procedure bcNotifyObservers(aSender: TObject;anOperation: TbcSubjectOperation;aData: pointer); { Notify all observers of a change }
+    property Extra: ptruint read fExtra write fExtra; { well, you never know :-) }
+    property Owner: TObject read fOwner write fOwner;
+  end; { TbcSubject }
 
+  { TbcObserver "I'm watching you" ~ observer pattern }
+  TbcObserver = class(TObject,IbcObserver)
+  protected
+    fOwner: TObject; { depends on the name of the observer, what class of object it is }
+  public
+    constructor Create(const anOwner: TObject);
+    destructor Destroy; override;
+    Procedure bcSubjectChanged(aSender: TObject;anOperation: TbcSubjectOperation;aData: pointer); virtual;
+    property Owner: TObject read fOwner write fOwner;
+  end; { TbcObserver }
 
 { factory provides a global object, singleton on demand }
 //Function Factory: TFactory;
@@ -62,6 +117,7 @@ type
 implementation
 
 {$i version.inc} { moved the implementation part to an include-file }
+{$i observer.inc} { moved the implementation part to an include-file }
 
 
 (*
